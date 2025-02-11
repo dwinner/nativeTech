cb2cc497c1a018c4831f5afa8cc3f2d1ff0e3122
Denis Vinevcev <dwinner@inbox.ru>
2025-02-11 19:02:58 +0300

    rust in action - completed

---
+++ rust/signals'n'interrupts/toy-global/src/main.rs
@@ -0,0 +1,22 @@
+use rand;
+
+static mut SHUT_DOWN: bool = false;
+
+fn main()
+{
+   loop
+   {
+      unsafe {
+         SHUT_DOWN = rand::random();
+      }
+
+      print!(".");
+
+      if unsafe { SHUT_DOWN }
+      {
+         break;
+      };
+   }
+
+   println!()
+}
