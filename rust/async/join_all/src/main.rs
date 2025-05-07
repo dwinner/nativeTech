9d684988915fb4a21926213e60e5b093d4751bf2
Denis Vinevcev <dwinner@inbox.ru>
2025-05-07 11:22:15 +0300

    rust - oop features

---
+++ rust/async/join_all/src/main.rs
@@ -0,0 +1,52 @@
+extern crate trpl;
+
+use std::pin::Pin;
+use std::time::Duration;
+
+fn main()
+{
+   trpl::run(async {
+      let (tx, mut rx) = trpl::channel();
+      let tx1 = tx.clone();
+      let tx1_fut = async move {
+         let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("future"),
+         ];
+
+         for val in vals
+         {
+            tx1.send(val).unwrap();
+            trpl::sleep(Duration::from_secs(1)).await;
+         }
+      };
+
+      let rx_fut = async {
+         while let Some(value) = rx.recv().await
+         {
+            println!("received '{value}'");
+         }
+      };
+
+      let tx_fut = async move {
+         let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+         ];
+
+         for val in vals
+         {
+            tx.send(val).unwrap();
+            trpl::sleep(Duration::from_secs(1)).await;
+         }
+      };
+
+      let futures: Vec<Pin<Box<dyn Future<Output = ()>>>> = vec![Box::pin(tx1_fut), Box::pin(rx_fut), Box::pin(tx_fut)];
+
+      trpl::join_all(futures).await;
+   });
+}
---
+++ rust/async/many_messages/src/main.rs
@@ -0,0 +1,28 @@
+extern crate trpl;
+
+use std::time::Duration;
+
+fn main()
+{
+   trpl::run(async {
+      let (tx, mut rx) = trpl::channel();
+
+      let vals = vec![
+         String::from("hi"),
+         String::from("from"),
+         String::from("the"),
+         String::from("future"),
+      ];
+
+      for val in vals
+      {
+         tx.send(val).unwrap();
+         trpl::sleep(Duration::from_millis(500)).await;
+      }
+
+      while let Some(value) = rx.recv().await
+      {
+         println!("received '{value}'");
+      }
+   });
+}
