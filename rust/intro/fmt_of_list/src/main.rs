d86fdd7c5a8a58136c51c747b4af1e28cefd8aa8
Denis Vinevcev <dwinner@inbox.ru>
2025-06-30 20:06:03 +0300

    rust by examples

---
+++ rust/intro/fmt_of_list/src/main.rs
@@ -0,0 +1,39 @@
+use std::fmt; // Import the `fmt` module.
+
+// Define a structure named `List` containing a `Vec`.
+struct List(Vec<i32>);
+
+impl fmt::Display for List
+{
+   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
+   {
+      // Extract the value using tuple indexing,
+      // and create a reference to `vec`.
+      let vec = &self.0;
+
+      write!(f, "[")?;
+
+      // Iterate over `v` in `vec` while enumerating the iteration
+      // index in `index`.
+      for (idx, val) in vec.iter().enumerate()
+      {
+         // For every element except the first, add a comma.
+         // Use the ? operator to return on errors.
+         if idx != 0
+         {
+            write!(f, ", ")?;
+         }
+
+         write!(f, "{}: {}", idx, val)?;
+      }
+
+      // Close the opened bracket and return a fmt::Result value.
+      write!(f, "]")
+   }
+}
+
+fn main()
+{
+   let v = List(vec![1, 2, 3]);
+   println!("{}", v);
+}
