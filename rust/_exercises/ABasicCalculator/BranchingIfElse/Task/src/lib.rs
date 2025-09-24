860281140839452c86c92e417a8dbec48b8d9bcd
Denis Vinevcev <dwinner@inbox.ru>
2025-09-24 09:09:17 +0300

    some changes

--- rust/_exercises/ABasicCalculator/BranchingIfElse/Task/src/lib.rs
+++ rust/_exercises/ABasicCalculator/BranchingIfElse/Task/src/lib.rs
@@ -1,6 +1,18 @@
 /// Return `12` if `n` is even,
 /// `13` if `n` is divisible by `3`,
 /// `17` otherwise.
-pub fn magic_number(n: u32) -> u32 {
-    /*TODO*/
+pub fn magic_number(n: u32) -> u32
+{
+   if (n % 2 == 0)
+   {
+      12
+   }
+   else if (n % 3 == 0)
+   {
+      13
+   }
+   else
+   {
+      17
+   }
 }
