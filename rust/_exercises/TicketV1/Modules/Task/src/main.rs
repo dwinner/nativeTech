a1f5844ce2c179c96e592af533147fad567b2da1
Denis Vinevcev <dwinner@inbox.ru>
2025-10-30 18:51:09 +0300

    rust - learning - memory layout

--- rust/_exercises/TicketV1/Modules/Task/src/main.rs
+++ rust/_exercises/TicketV1/Modules/Task/src/main.rs
@@ -1,44 +1,59 @@
-mod helpers {
-    // TODO: Make this code compile, either by adding a `use` statement or by using
-    //  the appropriate path to refer to the `Ticket` struct.
+mod helpers
+{
+   // TODO: Make this code compile, either by adding a `use` statement or by using
+   //  the appropriate path to refer to the `Ticket` struct.
 
-    /* TODO */
+   /* TODO */
+   use super::Ticket;
 
-    pub fn create_todo_ticket(title: String, description: String) -> Ticket {
-        Ticket::new(title, description, "To-Do".into())
-    }
+   pub fn create_todo_ticket(title: String, description: String) -> Ticket
+   {
+      Ticket::new(title, description, "To-Do".into())
+   }
 }
 
-struct Ticket {
-    title: String,
-    description: String,
-    status: String,
+struct Ticket
+{
+   title: String,
+   description: String,
+   status: String,
 }
 
-impl Ticket {
-    fn new(title: String, description: String, status: String) -> Ticket {
-        if title.is_empty() {
-            panic!("Title cannot be empty");
-        }
-        if title.len() > 50 {
-            panic!("Title cannot be longer than 50 bytes");
-        }
-        if description.is_empty() {
-            panic!("Description cannot be empty");
-        }
-        if description.len() > 500 {
-            panic!("Description cannot be longer than 500 bytes");
-        }
-        if status != "To-Do" && status != "In Progress" && status != "Done" {
-            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
-        }
+impl Ticket
+{
+   fn new(title: String, description: String, status: String) -> Ticket
+   {
+      if title.is_empty()
+      {
+         panic!("Title cannot be empty");
+      }
 
-        Ticket {
-            title,
-            description,
-            status,
-        }
-    }
+      if title.len() > 50
+      {
+         panic!("Title cannot be longer than 50 bytes");
+      }
+
+      if description.is_empty()
+      {
+         panic!("Description cannot be empty");
+      }
+
+      if description.len() > 500
+      {
+         panic!("Description cannot be longer than 500 bytes");
+      }
+
+      if status != "To-Do" && status != "In Progress" && status != "Done"
+      {
+         panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+      }
+
+      Ticket {
+         title,
+         description,
+         status,
+      }
+   }
 }
 
 fn main() {}
--- rust/_exercises/TicketV1/Ownership/Task/src/lib.rs
+++ rust/_exercises/TicketV1/Ownership/Task/src/lib.rs
@@ -3,49 +3,63 @@
 //   Change the existing implementation of `Ticket`'s accessor methods take a reference
 //   to `self` as an argument, rather than taking ownership of it.
 
-pub struct Ticket {
-    title: String,
-    description: String,
-    status: String,
+pub struct Ticket
+{
+   title: String,
+   description: String,
+   status: String,
 }
 
-impl Ticket {
-    pub fn new(title: String, description: String, status: String) -> Ticket {
-        if title.is_empty() {
-            panic!("Title cannot be empty");
-        }
-        if title.len() > 50 {
-            panic!("Title cannot be longer than 50 bytes");
-        }
-        if description.is_empty() {
-            panic!("Description cannot be empty");
-        }
-        if description.len() > 500 {
-            panic!("Description cannot be longer than 500 bytes");
-        }
-        if status != "To-Do" && status != "In Progress" && status != "Done" {
-            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
-        }
+impl Ticket
+{
+   pub fn new(title: String, description: String, status: String) -> Ticket
+   {
+      if title.is_empty()
+      {
+         panic!("Title cannot be empty");
+      }
 
-        Ticket {
-            title,
-            description,
-            status,
-        }
-    }
+      if title.len() > 50
+      {
+         panic!("Title cannot be longer than 50 bytes");
+      }
+
+      if description.is_empty()
+      {
+         panic!("Description cannot be empty");
+      }
+
+      if description.len() > 500
+      {
+         panic!("Description cannot be longer than 500 bytes");
+      }
+
+      if status != "To-Do" && status != "In Progress" && status != "Done"
+      {
+         panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+      }
+
+      Ticket {
+         title,
+         description,
+         status,
+      }
+   }
 
    // TODO:
-    pub fn title(self) -> String {
-        self.title
-    }}
+   pub fn title(&self) -> &String
+   {
+      &self.title
+   }
 
    // TODO:
-    pub fn description(self) -> String {
-        self.description
-    }}
+   pub fn description(&self) -> &String
+   {
+      &self.description
+   }
 
-   // TODO:
-    pub fn status(self) -> String {
-        self.status
-    }}
+   pub fn status(&self) -> &String
+   {
+      &self.status
+   }
 }
