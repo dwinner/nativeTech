dfb8767f585b6e9f190152e1039b9afc49b2059f
Denis Vinevcev <dwinner@inbox.ru>
2025-11-17 22:54:14 +0300

    Rust ex. - vectors

--- rust/_exercises/TicketManagement/Iterators/Task/src/lib.rs
+++ rust/_exercises/TicketManagement/Iterators/Task/src/lib.rs
@@ -9,48 +9,65 @@ use ticket_fields::{TicketDescription, TicketTitle};
 //   Look at the standard library documentation for `Vec` to find the right type
 //   to return from `into_iter`.
 #[derive(Clone)]
-pub struct TicketStore {
-    tickets: Vec<Ticket>,
+pub struct TicketStore
+{
+   tickets: Vec<Ticket>,
 }
 
 #[derive(Clone, Debug, PartialEq)]
-pub struct Ticket {
-    pub title: TicketTitle,
-    pub description: TicketDescription,
-    pub status: Status,
+pub struct Ticket
+{
+   pub title: TicketTitle,
+   pub description: TicketDescription,
+   pub status: Status,
 }
 
-impl Ticket {
-    pub fn new(title: TicketTitle, description: TicketDescription, status: Status) -> Self {
-        Self {
-            title,
-            description,
-            status,
-        }
-    }
+impl Ticket
+{
+   pub fn new(title: TicketTitle, description: TicketDescription, status: Status) -> Self
+   {
+      Self {
+         title,
+         description,
+         status,
+      }
+   }
 }
 
 #[derive(Clone, Debug, Copy, PartialEq)]
-pub enum Status {
-    ToDo,
-    InProgress,
-    Done,
+pub enum Status
+{
+   ToDo,
+   InProgress,
+   Done,
 }
 
-impl TicketStore {
-    pub fn new() -> Self {
-        Self {
-            tickets: Vec::new(),
-        }
-    }
+impl TicketStore
+{
+   pub fn new() -> Self
+   {
+      Self { tickets: Vec::new() }
+   }
 
-    pub fn add_ticket(&mut self, ticket: Ticket) {
-        self.tickets.push(ticket);
-    }
+   pub fn add_ticket(&mut self, ticket: Ticket)
+   {
+      self.tickets.push(ticket);
+   }
 
-    pub fn tickets(&self) -> &Vec<Ticket> {
-        &self.tickets
-    }
+   pub fn tickets(&self) -> &Vec<Ticket>
+   {
+      &self.tickets
+   }
 }
 
-i/* TODO */
+/* TODO */
+impl IntoIterator for TicketStore
+{
+   type Item = Ticket;
+   type IntoIter = std::vec::IntoIter<Self::Item>;
+
+   fn into_iter(self) -> Self::IntoIter
+   {
+      self.tickets.into_iter()
+   }
+}
