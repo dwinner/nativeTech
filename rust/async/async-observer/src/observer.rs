c8dac8067d5ec8bd6be3c9b9b2c466bbac8cb56d
Denis Vinevcev <dwinner@inbox.ru>
2026-01-15 22:38:50 +0300

    observer impl. for Rust

---
+++ rust/async/async-observer/src/observer.rs
@@ -0,0 +1,8 @@
+use std::{future::Future, pin::Pin};
+
+pub trait Observer: Send + Sync
+{
+   type Subject;
+   type Output;
+   fn observe<'a>(&'a self, subject: &'a Self::Subject) -> Pin<Box<dyn Future<Output = Self::Output> + 'a + Send>>;
+}
