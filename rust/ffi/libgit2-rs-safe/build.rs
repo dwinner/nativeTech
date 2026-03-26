17bc78263d2cc0044e9cd1f601dd4a80d2373e0d
Denis Vinevcev <dwinner@inbox.ru>
2026-03-26 22:17:37 +0300

    ffi in rust - done

--- rust/ffi/libgit2-rs-safe/build.rs
+++ rust/ffi/libgit2-rs-safe/build.rs
@@ -1,3 +1,4 @@
-fn main() {
-    println!(r"cargo:rustc-link-search=native=c:\vcpkg\packages\libgit2_x64-windows\lib");
+fn main()
+{
+   println!(r"cargo:rustc-link-search=native=c:\vcpkg\packages\libgit2_x64-windows\lib");
 }
