0eb5dd15f7c6943a05884b965951ac09d11b25c3
Denis Vinevcev <dwinner@inbox.ru>
2026-04-02 11:20:41 +0300

    idioms

---
+++ rust/functional/functional-tour/src/main.rs
@@ -0,0 +1,44 @@
+use std::collections::LinkedList;
+
+fn main()
+{
+   let arr = [1, 2, 3, 4];
+   println!("{:?}", arr);
+   let vec: Vec<_> = arr.iter().map(|val| val.to_string()).collect();
+   println!("{:?}", vec);
+
+   let linked_list: LinkedList<i32> = vec.iter().flat_map(|val| val.parse::<i32>()).collect();
+   println!("{:?}", linked_list);
+
+   let arr = ["duck", "1", "2", "goose", "3", "4"];
+   let (successes, failures): (Vec<_>, Vec<_>) = arr.iter().map(|val| val.parse::<i32>()).partition(Result::is_ok);
+   println!("successses={:?}", successes);
+   println!("failures={:?}", failures);
+
+   let successes: Vec<_> = successes.into_iter().map(Result::unwrap).collect();
+   let failures: Vec<_> = failures.into_iter().map(Result::unwrap_err).collect();
+   println!("successses={:?}", successes);
+   println!("failures={:?}", failures);
+
+   let popular_dog_breeds = vec![
+      "Labrador",
+      "French Bulldog",
+      "Golden Retriever",
+      "German Shepherd",
+      "Poodle",
+      "Bulldog",
+      "Beagle",
+      "Rottweiler",
+      "Pointer",
+      "Dachshund",
+   ];
+
+   let ranked_breeds: Vec<_> = popular_dog_breeds
+      .into_iter()
+      .enumerate()
+      .map(|(idx, breed)| (idx + 1, breed))
+      .rev()
+      .collect();
+
+   println!("{:?}", ranked_breeds);
+}
---
+++ rust/smart-pointers/global-vars/src/main.rs
@@ -0,0 +1,67 @@
+use lazy_static::lazy_static;
+use once_cell::sync::Lazy;
+use static_init::dynamic;
+use std::cell::{OnceCell, RefCell};
+use std::rc::Rc;
+
+thread_local! {
+    static POPULAR_BABY_NAMES_2021: Rc<RefCell<Vec<String>>> = Rc::new(RefCell::new(Vec::new()));
+}
+
+lazy_static! {
+   static ref POPULAR_BABY_NAMES_2020: Vec<String> = {
+      vec![
+         String::from("Olivia"),
+         String::from("Liam"),
+         String::from("Emma"),
+         String::from("Noah"),
+      ]
+   };
+}
+
+static POPULAR_BABY_NAMES_2019: Lazy<Vec<String>> = Lazy::new(|| {
+   vec![
+      String::from("Olivia"),
+      String::from("Liam"),
+      String::from("Emma"),
+      String::from("Noah"),
+   ]
+});
+
+#[dynamic]
+static POPULAR_BABY_NAMES_2018: Vec<String> = vec![
+   String::from("Emma"),
+   String::from("Liam"),
+   String::from("Olivia"),
+   String::from("Noah"),
+];
+
+fn main()
+{
+   POPULAR_BABY_NAMES_2021.with(|rc| {
+      rc.borrow_mut().extend([
+         "Olivia".to_string(),
+         "Liam".to_string(),
+         "Emma".to_string(),
+         "Noah".to_string(),
+      ]);
+   });
+
+   let popular_baby_names_2017: OnceCell<Vec<String>> = OnceCell::new();
+   popular_baby_names_2017.get_or_init(|| {
+      vec![
+         String::from("Emma"),
+         String::from("Liam"),
+         String::from("Olivia"),
+         String::from("Noah"),
+      ]
+   });
+
+   let names = POPULAR_BABY_NAMES_2021.with(|rc| rc.borrow().clone());
+
+   println!("popular baby names of 2021: {:?}", names);
+   println!("popular baby names of 2020: {:?}", *POPULAR_BABY_NAMES_2020);
+   println!("popular baby names of 2019: {:?}", *POPULAR_BABY_NAMES_2019);
+   println!("popular baby names of 2018: {:?}", *POPULAR_BABY_NAMES_2018);
+   println!("popular baby names of 2017: {:?}", popular_baby_names_2017.get());
+}
