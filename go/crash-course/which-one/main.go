25a9d4018e527c38244519b045889137a5e35103
vinevtsev <vinevtsev@schleissheimer.de>
2026-05-19 11:02:02 +0200

    Update main.go

--- go/crash-course/which-one/main.go
+++ go/crash-course/which-one/main.go
@@ -10,13 +10,14 @@ func main() {
 	arguments := os.Args
 	if len(arguments) == 1 {
 		fmt.Println("Please provide an argument")
-		return
+		os.Exit(1)
 	}
 
 	file := arguments[1]
 	path := os.Getenv("PATH")
 	pathSplit := filepath.SplitList(path)
 
+	allPaths := make([]string, 0)
 	for _, directory := range pathSplit {
 		fullPath := filepath.Join(directory, file)
 
@@ -33,10 +34,10 @@ func main() {
 			continue
 		}
 
-		// Is it executable?
-		if mode&0111 != 0 {
-			fmt.Println(fullPath)
-			return
-		}
+		allPaths = append(allPaths, fullPath)
+	}
+
+	for _, path := range allPaths {
+		fmt.Println(path)
 	}
 }
