d18f260264f6dadbd0803e8dbf44fc99d2e941e8
Denis Vinevcev <dwinner@inbox.ru>
2026-07-31 08:21:36 +0300

    update gitignore

--- go/interfaces/generic-interfaces/main.go
+++ go/interfaces/generic-interfaces/main.go
@@ -1,42 +1,42 @@
 package main
 
 import (
-   "fmt"
+	"fmt"
 )
 
 type Numeric interface {
-   int | int8 | int16 | int32 | int64 | float64
+	int | int8 | int16 | int32 | int64 | float64
 }
 
 func Print(iface interface{}) {
-   // type switch
-   switch iface.(type) {
-   case int:
-      fmt.Println(iface.(int) + 1)
-   case float64:
-      fmt.Println(iface.(float64) + 1)
-   default:
-      fmt.Println("Unknown data type!")
-   }
+	// type switch
+	switch iface := iface.(type) {
+	case int:
+		fmt.Println(iface + 1)
+	case float64:
+		fmt.Println(iface + 1)
+	default:
+		fmt.Println("Unknown data type!")
+	}
 }
 
 func PrintGenerics[T any](anAny T) {
-   fmt.Println(anAny)
+	fmt.Println(anAny)
 }
 
 func PrintNumeric[T Numeric](aNumber T) {
-   fmt.Println(aNumber + 1)
+	fmt.Println(aNumber + 1)
 }
 
 func main() {
-   Print(12)
-   Print(-1.23)
-   Print("Hi!")
+	Print(12)
+	Print(-1.23)
+	Print("Hi!")
 
-   PrintGenerics(1)
-   PrintGenerics("a")
-   PrintGenerics(-2.33)
+	PrintGenerics(1)
+	PrintGenerics("a")
+	PrintGenerics(-2.33)
 
-   PrintNumeric(1)
-   PrintNumeric(-2.33)
+	PrintNumeric(1)
+	PrintNumeric(-2.33)
 }
