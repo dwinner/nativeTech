20bbb63a7f4434bb632147dfb489d8896afb9407
Denis Vinevcev <dwinner@inbox.ru>
2026-09-18 18:03:58 +0300

    template engine v1 - done

--- rust/_projects/template-engine-v1/src/main.rs
+++ rust/_projects/template-engine-v1/src/main.rs
@@ -3,14 +3,18 @@ use std::io;
 use std::io::BufRead;
 use template_engine_v1::*;
 
-fn main() {
+fn main()
+{
    let mut context: HashMap<String, String> = HashMap::new();
    context.insert("name".to_string(), "Bob".to_string());
    context.insert("city".to_string(), "Boston".to_string());
 
-   for line in io::stdin().lock().lines() {
-      match get_content_type(&line.unwrap().clone()) {
-         ContentType::TemplateVariable(content) => {
+   for line in io::stdin().lock().lines()
+   {
+      match get_content_type(&line.unwrap().clone())
+      {
+         ContentType::TemplateVariable(content) =>
+         {
             let html = generate_html_template_var(content, context.clone());
             println!("{}", html);
          }
